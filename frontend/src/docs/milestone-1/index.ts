ed66b1f08a8d01fa12ace376aa7fdcbcab480403
Atharva Kale <atharvakale343@gmail.com>
2024-03-14 18:44:03 -0400

    animated scrolling

--- frontend/src/docs/milestone-1/index.ts
+++ frontend/src/docs/milestone-1/index.ts
@@ -4,5 +4,9 @@ import '../../styles/global.css';
 document
   .getElementsByClassName('back-to-top')[0]
   .addEventListener('click', () => {
-    window.scrollTo(0, 0);
+    // animate this scrolling
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
   });
