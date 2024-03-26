330865ed7f25f9a893a3eb3d3680f9b2a7c9cd96
Atharva Kale <atharvakale343@gmail.com>
2024-03-26 19:31:57 -0400

    buttons, rubric milestone 1

--- frontend/src/docs/milestone-1/index.ts
+++ frontend/src/docs/milestone-1/index.ts
@@ -1,12 +1,22 @@
 import '../../styles/global.scss';
 import '../../styles/global.css';
 
-document
-  .getElementsByClassName('back-to-top')[0]
-  .addEventListener('click', () => {
-    // animate this scrolling
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
+// @ts-ignore
+document.getElementById('back-to-top').addEventListener('click', () => {
+  // animate this scrolling
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
   });
+});
+
+// take the element cookie-buttom and add an event listener to it, update its counter value
+const cookieButton = document.getElementById(
+  'cookie-button'
+) as HTMLButtonElement;
+let counter = 0;
+
+cookieButton.addEventListener('click', () => {
+  counter++;
+  cookieButton.innerText = `🍪 Counter: ${counter}`;
+});
