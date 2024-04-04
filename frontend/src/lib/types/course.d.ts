98cad6622bd5008950b19efc10214b3097b5bb00
Atharva Kale <atharvakale343@gmail.com>
2024-04-04 17:09:30 -0400

    course type

---
+++ frontend/src/lib/types/course.d.ts
@@ -0,0 +1,11 @@
+type Course = {
+    courseSubjectId: string;    // looks like "COMPSCI"
+    courseNumber: string;       // looks like "187"
+    courseTitle: string;
+    semester: string;
+    credits: number;
+    professor: string;
+    grade: string;
+    notes: string;
+    courseDescription: string;
+}
