ec9b93cc0e81dd00bf53cd46eccef98a5fc074b9
Atharva Kale <atharvakale343@gmail.com>
2024-04-14 19:04:07 -0400

    responsive

--- frontend/src/client/CourseHistory.ts
+++ frontend/src/client/CourseHistory.ts
@@ -68,7 +68,7 @@ export class CourseHistory {
       </button>
       <p class="text-black">You have completed the following courses:</p>
 
-      <div class="relative grid grid-cols-3 gap-4">
+      <div class="relative grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         ${sortedSemesters
           .map(
             (semester) => /* HTML */ `
