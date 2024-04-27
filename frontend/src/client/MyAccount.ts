b5f582fe85436c508294919ab5fe21d41ff9ae5b
Atharva Kale <atharvakale343@gmail.com>
2024-04-27 19:02:24 -0400

    margin

--- frontend/src/client/MyAccount.ts
+++ frontend/src/client/MyAccount.ts
@@ -197,7 +197,7 @@ export class MyAccount {
     elm.classList.add('my-4', 'mx-4', 'fade-in-element', 'relative');
     elm.innerHTML = /* HTML */ `
       <div
-        class="account-form m-auto h-40 max-w-2xl flex flex-col items-center gap-y-8 rounded-lg bg-slate-50 p-8 shadow-md md:gap-y-8"
+        class="login-buttons mb-32 m-auto h-40 max-w-2xl flex flex-col items-center gap-y-8 rounded-lg bg-slate-50 p-8 shadow-md md:gap-y-8"
       >
         <h1 class="text-2xl font-bold text-black dark:text-white">
           Sign in to CourseView
